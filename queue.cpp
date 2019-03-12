93b23adbd8a1ed9831ce6140c0261ec9b1257d2a
JLVlo <j.lipski4@gmail.com>
2019-03-12 12:28:21 +0100

    Add queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	char sign;
+	queue<int> q;
+
+	while (cin >> sign)
+	{
+		if (sign == '+')
+		{
+			cin >> number;
+			q.push(number);
+		}
+		else
+		{
+			if (sign == '-')
+			{
+				if (!q.empty())
+				{
+					q.pop();
+				}
+				else
+				{
+					cout << "Error" << endl;
+				}
+			}
+			else
+			{
+				cout << "What is ...?" << endl;
+			}
+		}
+	}
+	
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
+	}
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
